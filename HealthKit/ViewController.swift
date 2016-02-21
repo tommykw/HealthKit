b9bf03ccc417d4177e70d51f277f943e0e5baa79
kenji tomita <destinys.lotus.1108mn@gmail.com>
2016-02-22 06:51:56 +0900

    first commit

---
+++ HealthKit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HealthKit
+//
+//  Created by tomitakenji on 2016/02/22.
+//  Copyright © 2016年 kenji tomita. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
